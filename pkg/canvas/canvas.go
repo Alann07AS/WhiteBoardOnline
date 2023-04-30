09a73900b0cf64842f7eafa5581c6281b4ee11ed
Alann Schnebelen <122521922+Alann07AS@users.noreply.github.com>
2023-04-30 11:23:12 +0200

    Update canvas.go add ctx init , point and clear

--- pkg/canvas/canvas.go
+++ pkg/canvas/canvas.go
@@ -1,1 +1,41 @@
 package canvas
+
+import (
+
+    "github.com/tfriedel6/canvas"
+
+)
+
+var ctx *canvas.Context
+
+func InitCanvas(width, height int) *canvas.Canvas {
+
+    // Création d'un canevas de la taille spécifiée
+
+    cnv := canvas.New(float64(width), float64(height))
+
+    // Récupération du contexte du canevas
+
+    ctx = cnv.Context2D()
+
+    return cnv
+
+}
+
+func Point(x, y, r float64, color string) {
+
+    ctx.BeginPath()
+
+    ctx.FillStyle = color
+
+    ctx.Arc(x, y, r, 0, 2*math.Pi, false)
+
+    ctx.Fill()
+
+}
+
+func Clear() {
+
+    ctx.ClearRect(0, 0, ctx.Width(), ctx.Height())
+
+}
